1cd11e3fbfae91698e6cf28fefe74f8ebab81113
l7960261 <l7960261@gmail.com>
2018-10-18 16:35:50 +0800

    complete fizzbuzz.spec.js

--- src/domain/fizzbuzz.js
+++ src/domain/fizzbuzz.js
@@ -1,5 +1,13 @@
 export class FizzBuzz {
-    print(data) {
-        return data;
+    print(input) {
+        if (input % 15 === 0) {
+            return 'FizzBuzz';
+        } else if (input % 3 === 0) {
+            return 'Fizz';
+        } else if (input % 5 === 0) {
+            return 'Buzz';
+        }
+
+        return input.toString();
     }
 }
