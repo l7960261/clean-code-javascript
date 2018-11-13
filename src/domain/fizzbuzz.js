fd4e7acbbf1f73482228ff0b3b393423bf25371f
l7960261 <l7960261@gmail.com>
2018-11-13 14:05:07 +0800

    init fizzbuzz and spec

--- src/domain/fizzbuzz.js
+++ src/domain/fizzbuzz.js
@@ -1,16 +1,5 @@
 export class FizzBuzz {
-    print(input) {
-        const TIMES_OF_15 = 15;
-        const TIMES_OF_5 = 5;
-        const TIMES_OF_3 = 3;
-        const NORMAL_NUMBER = 1;
-        const resource = [
-            [TIMES_OF_15, () => 'FizzBuzz'],
-            [TIMES_OF_5, () => 'Buzz'],
-            [TIMES_OF_3, () => 'Fizz'],
-            [NORMAL_NUMBER, () => input.toString()]
-        ];
-
-        return resource.find(divisor => input % divisor[0] === 0)[1]();
-    }
+  print(input) {
+    return input.toString();
+  }
 }
--- src/tests/fizzbuzz.spec.js
+++ src/tests/fizzbuzz.spec.js
@@ -1,30 +1,15 @@
 import {
-    FizzBuzz
+  FizzBuzz
 } from "../domain/fizzbuzz";
 
 describe('fizzbuzz', () => {
-    let instance;
-    beforeEach(() => {
-        instance = new FizzBuzz();
-    });
+  let instance;
+  beforeEach(() => {
+    instance = new FizzBuzz();
+  });
 
-    it('pass any normal number should print input number', () => {
-        expect(instance.print(1)).toBe('1');
-        expect(instance.print(2)).toBe('2');
-    });
-
-    it('pass 3 should print Fizz', () => {
-        expect(instance.print(3)).toBe('Fizz');
-        expect(instance.print(6)).toBe('Fizz');
-    });
-
-    it('pass 5 should print Buzz', () => {
-        expect(instance.print(5)).toBe('Buzz');
-        expect(instance.print(10)).toBe('Buzz');
-    });
-
-    it('pass 15 should print FizzBuzz', () => {
-        expect(instance.print(15)).toBe('FizzBuzz');
-        expect(instance.print(30)).toBe('FizzBuzz');
-    });
+  it('pass any normal number should print input number', () => {
+    expect(instance.print(1)).toBe('1');
+    expect(instance.print(2)).toBe('2');
+  });
 });
