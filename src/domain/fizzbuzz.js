529d6ad6ef437746a7336a7b0c27363a598adf69
l7960261 <l7960261@gmail.com>
2018-11-15 21:37:28 +0800

    compelete fizzbuzz.spec.js

--- src/domain/fizzbuzz.js
+++ src/domain/fizzbuzz.js
@@ -1,5 +1,13 @@
 export class FizzBuzz {
   print(input) {
+    if (input % 15 === 0) {
+      return 'FizzBuzz';
+    } else if (input % 5 === 0) {
+      return 'Buzz';
+    } else if (input % 3 === 0) {
+      return 'Fizz';
+    }
+
     return input.toString();
   }
 }
